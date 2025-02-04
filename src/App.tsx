bca2911acc9ff8112cc82ed7d2f006a517ad5728
Md. Rayhan Shorker <raihanshorker56@gmail.com>
2025-02-04 13:26:32 +0600

    initial setup react,typescript,shadcn ui,tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+<div>
+  <Button className="bg-[#3279EA]">CLick</Button>
+</div>
+  )
+}
+
+export default App
